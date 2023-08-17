b969161260b103f5c43d14f537812cfd54c7bc70
Anthony Jimenez <anthony.j.p_01@hotmail.com>
2023-08-17 15:02:31 -0500

    Agregar archivos de proyecto.

---
+++ JWTProject/Constants/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿using JWTProject.Models;
+
+namespace JWTProject.Constants
+{
+    public class UserConstants
+    {
+        public static List<UserModels> Users = new List<UserModels>()
+        {
+            new UserModels(){Username ="admin",Password="admin",Rol="Administrador", EmailAddress="admin@admin.com", FirstName="Admin", LastName="Second"},
+            new UserModels() { Username ="user",Password="user",Rol="Usuario", EmailAddress="user@user.com", FirstName="User", LastName="Second"}
+            };
+    }
+}
---
+++ JWTProject/Models/UserModels.cs
@@ -0,0 +1,13 @@
+﻿namespace JWTProject.Models
+{
+    public class UserModels
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Rol { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+    }
+}
